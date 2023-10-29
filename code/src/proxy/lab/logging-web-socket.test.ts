840b685d388c6cd65814ca3bca0d9eb80ce63980
patcharachart jirasrisopa <patcharachart@odds.team>
2023-10-29 11:28:26 +0700

    proxy pattern :bee:

--- code/src/proxy/lab/logging-web-socket.test.ts
+++ code/src/proxy/lab/logging-web-socket.test.ts
@@ -1,5 +1,6 @@
 import WebSocket from 'ws';
 import LoggingWebSocket from './logging-web-socket';
+  import { WebSocketConnectImpl } from './websocket-connect';
 
 jest.mock('ws');
 describe('[Proxy - lab] LoggingWebSocket', function () {
@@ -17,4 +18,28 @@ describe('[Proxy - lab] LoggingWebSocket', function () {
       'Monday, October 2, 2023 at 07:00:00 AM GMT+7'
     );
   });
+
+  it('should return date time in human-readable format with tomorrow date', function () {
+    // given
+    const loggingWebSocket = new LoggingWebSocket('ws://example.com/socket');
+    jest.useFakeTimers().setSystemTime(new Date('2023-10-02'));
+    loggingWebSocket.connect();
+
+    // when
+    loggingWebSocket.send('tomorrow');
+
+    // then
+    expect(WebSocket.prototype.send).toHaveBeenCalledWith(
+      'Tuesday, October 3, 2023 at 07:00:00 AM GMT+7'
+    );
+  });
+
+  it('should throw error when web socket is not connect', () => {
+    const loggingWebSocket = new WebSocketConnectImpl('ws://example.com/socket');
+
+    const send = () => loggingWebSocket.send('Hello');
+
+    expect(send).toThrowError('WebSocket is not connected.')
+  });
+  
 });
--- code/src/proxy/lab/logging-web-socket.ts
+++ code/src/proxy/lab/logging-web-socket.ts
@@ -1,17 +1,17 @@
-import WebSocket from 'ws';
+import { WebSocketConnect, WebSocketConnectImpl } from './websocket-connect';
 
 class LoggingWebSocket {
   private readonly url: string;
-  private socket: WebSocket | null;
+  private socket: WebSocketConnect | null;
 
   constructor(url: string) {
     this.url = url;
-    this.socket = null;
+    this.socket = new WebSocketConnectImpl(this.url);
   }
 
   connect() {
     try {
-      this.socket = new WebSocket(this.url);
+      this.socket?.connect();
     } catch (e) {
       console.error(e);
     }
@@ -20,30 +20,39 @@ class LoggingWebSocket {
   send(data: string) {
     if (this.socket) {
       if (data === 'now') {
-        const date = new Date();
-
-        const options: Intl.DateTimeFormatOptions = {
-          weekday: 'long',
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric',
-          hour: '2-digit',
-          minute: '2-digit',
-          second: '2-digit',
-          timeZoneName: 'short',
-        };
-
-        const formattedDate: string = new Intl.DateTimeFormat(
-          'en-US',
-          options,
-        ).format(date);
-
-        data = `${formattedDate}`;
+        const formattedDate: string = this.tranformTimeToReadableFormat();
+        data = formattedDate;
+      }
+      if (data === 'tomorrow') {
+        const date: Date = new Date();
+        date.setDate(date.getDate() + 1);
+        const formattedDate: string = this.tranformTimeToReadableFormat(date);
+        data = formattedDate;
       }
       this.socket.send(data);
     } else {
       console.error('WebSocket is not connected.');
-    }
+    };
+  }
+
+  tranformTimeToReadableFormat(date: Date = new Date()): string {
+    const options: Intl.DateTimeFormatOptions = {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      timeZoneName: 'short',
+    };
+
+    const formattedDate: string = new Intl.DateTimeFormat(
+      'en-US',
+      options,
+    ).format(date);
+
+    return `${formattedDate}`;
   }
 }
 
